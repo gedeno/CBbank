cbcb50725ef65d8fb232178755c4740ebb3d262e
gedeno <gedenogeremew@gmail.com>
2026-03-15 16:09:12 +0300

    many files are imported on views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCrationform 
+from django.views.generic import FormView
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import TemplateView
+from django.views import generic
+from django.views.generic.detail import DetailView
+from django.contrib.auth.models import User
 
 # Create your views here.
